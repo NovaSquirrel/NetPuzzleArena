e723020f67582c21fd74695704654391187bae8f
NovaSquirrel <bushytail.squirrel@gmail.com>
2018-11-07 03:14:27 -0500

    Credit in the readme for accurate Puzzle League

--- src/frenzyct.c
+++ src/frenzyct.c
@@ -1,4 +1,22 @@
-// Original Tetris Attack implementation by Colour Thief, adapted here
+/*
+ * Net Puzzle Arena
+ *
+ * Copyright (C) 2016-2018 NovaSquirrel
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+// Original Tetris Attack implementation by colour_thief, adapted and modified here
 #include "puzzle.h"
 
 #define HOVER 12
